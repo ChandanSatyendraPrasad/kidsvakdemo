05c5100d10d3d1e0eb2602bc13e336a58ef4f06b
ChandanSatyendraPrasad <chandansatyendraprasad@gmail.com>
2022-04-20 05:19:30 +0530

    Initial commit

---
+++ app/src/main/java/kids/vak/cwd/fragment/DrawingFragment.java
@@ -0,0 +1,35 @@
+package kids.vak.cwd.fragment;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import kids.vak.cwd.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DrawingFragment extends Fragment {
+
+
+    public DrawingFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_drawing, container, false);
+
+        return view;
+    }
+
+
+}
